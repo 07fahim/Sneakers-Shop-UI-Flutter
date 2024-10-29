362a08c4e9cab2c0aa82be4b3d16c365ce14bf96
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-10-30 02:34:36 +0600

    first commit

---
+++ lib/pages/sneakers_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SneakersPage extends StatelessWidget {
+  const SneakersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
