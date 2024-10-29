fdce2ceeef7d3cef1b39ee3de47bb1cb732694a7
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-10-30 02:45:03 +0600

    Stateful

--- lib/pages/sneakers_page.dart
+++ lib/pages/sneakers_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SneakersPage extends StatelessWidget {
+class SneakersPage extends StatefulWidget {
   const SneakersPage({super.key});
 
+  @override
+  State<SneakersPage> createState() => _SneakersPageState();
+}
+
+class _SneakersPageState extends State<SneakersPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
